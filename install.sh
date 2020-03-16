e230a7bd051ec46e817dffba5ed1b28ff222a997
Bl4ckCl0v3r <37512678+Bl4ckCl0v3r@users.noreply.github.com>
2020-03-16 16:54:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 termux-setup-storage
-pkg update -y && pkg upgrade -y && pkg install php git python ruby curl coreutils figlet -y && gem install lolcat -y
+pkg update -y && pkg upgrade -y && pkg install php git python ruby curl coreutils figlet nano -y && gem install lolcat -y
