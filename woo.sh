0fcb04fc5611f9feadb2bf4b2ca25b984cf6f671
Bl4ckCl0v3r <37512678+Bl4ckCl0v3r@users.noreply.github.com>
2020-03-16 07:11:23 +0000

    Update woo.sh

--- woo.sh
+++ woo.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 echo "Masukkan No Polisi/No Rangka/No Mesin" | lolcat
-read -p "" nopol
-if result=$(grep -i "$nopol" storage/downloads/wo/*.txt)
+echo "--------------"
+read -p "   " nopol
+echo "--------------"
+if result=$(grep -i "$nopol" storage/downloads/wo/*.t>
 then
 echo "$result"
 else
-echo "No Polisi/No Rangka/No Mesin Tidak Terdeteksi" | lolcat
+echo "No Polisi/No Rangka/No Mesin Tidak Terdeteksi" >
 fi
+echo "--------------"
